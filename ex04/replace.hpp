4bfee4cd33da5380f36adfb713394644af078698
mnaumann <naumannlinguae@gmail.com>
2025-02-13 17:05:34 +0100

    started ex04 and provided test texts

---
+++ ex04/replace.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   replace.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnaumann <mnaumann@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/13 16:54:13 by mnaumann          #+#    #+#             */
+/*   Updated: 2025/02/13 16:59:35 by mnaumann         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef REPLACE_HPP
+# define REPLACE_HPP
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#endif
+
